c6c52434f50b44f17035f171511dc68b67f99f3b
Fumio Hashidume <hassy@air.ne.jp>
2015-09-23 21:56:05 +0900

    modify

--- appbank/spiders/pdmon2.py
+++ appbank/spiders/pdmon2.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -14,21 +15,24 @@ class Pdmon2Spider(CrawlSpider):
 
     rules = (
         #Rule(LinkExtractor(allow=r'/m\d\d\d$'), callback='parse_item', follow=True),
-        Rule(LinkExtractor(allow=r'/m226\d'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'/m226[0-2]'), callback='parse_item', follow=True),
         #Rule(LinkExtractor(allow=r'/m\d\d\d.php'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-	#title = response.css('h3::text').extract_first()
-	#item = AppbankItem(title=title)
-        name = response.css('div#monster h3.title-pattern-darkblue::text').extract_first()
-        #stars = response.css('div#monster div.detail p.icon-attr::text').extract()
+        no_and_name = response.css('div#monster h3.title-pattern-darkblue::text').extract()
+        regex = u'No.(?P<num>[\d]+)\s(?P<str>.*$)'
+        match = re.search(regex, no_and_name[0], re.U)
+        mon_no = match.group('num')
+        name = match.group('str')
         props = response.css('div#monster div.detail p.icon-attr i[class*=icon]::attr(class)').extract()
         types = response.css('div#monster div.detail p.icon-mtype i[class*=icon-mtype]::attr(class)').extract()
 	item = AppbankItem(
+                mon_no = mon_no,
                 name = name,
-                main_prop = props,
+                main_prop = props[0],
+                sub_prop = props[1] if len(props) > 1 else "",
                 main_type = types,
+                sub_type = types[1] if len(types) > 1 else "",
         )
-                #stars=stars,
 	return item
